90cfac9720e5f4fdeaa9dc0389b01f94e86048c2
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-16 23:30:22 +0000

    Redirect logged-in users away from auth pages

---
+++ src/app/(public)/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth";
+import { getDashboardPath } from "@/lib/routes";
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser();
+
+  if (user) {
+    redirect(getDashboardPath());
+  }
+
+  return <>{children}</>;
+}
