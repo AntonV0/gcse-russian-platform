b94896301980a5907b8dd1dd64f2e57a0a939eec
AntonV0 <anton.vlasenko94@gmail.com>
2026-04-25 14:22:37 +0100

    refactor(layout): soften logout button styling

--- src/components/layout/logout-button.tsx
+++ src/components/layout/logout-button.tsx
@@ -6,7 +6,7 @@ import Button from "@/components/ui/button";
 export default function LogoutButton() {
   return (
     <form action={signOut}>
-      <Button type="submit" variant="danger" size="sm" icon="userX">
+      <Button type="submit" variant="exit" size="sm" icon="userX">
         Log out
       </Button>
     </form>
